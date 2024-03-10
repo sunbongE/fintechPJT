308ca7a6139afdff198d9d6bab0a16a5eb5bc5a5
김민준 <yid0816@naver.com>
2024-03-10 23:53:30 +0900

    setting: flutter

---
+++ FrontEnd/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:front/screen/HomeScreen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    ),
+  );
+}
