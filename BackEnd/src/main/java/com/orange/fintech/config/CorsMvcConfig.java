f40064872a567842ab9adf5026b0976d8051bbb7
박태호 <qkrxogh7@naver.com>
2024-03-15 16:19:01 +0900

    refactor : cors 5000 추가

--- BackEnd/src/main/java/com/orange/fintech/config/CorsMvcConfig.java
+++ BackEnd/src/main/java/com/orange/fintech/config/CorsMvcConfig.java
@@ -13,6 +13,6 @@ public class CorsMvcConfig implements WebMvcConfigurer {
         corsRegistry
                 .addMapping("/**")
                 .exposedHeaders("Set-Cookie")
-                .allowedOrigins("http://localhost:3000");
+                .allowedOrigins("http://localhost:5000");
     }
 }
