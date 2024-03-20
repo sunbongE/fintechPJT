7a683c2a3ed13b930d273c1e1b5cc11e7ec7fe49
김신영 <estumav@gmail.com>
2024-03-20 11:49:57 +0900

    refactor: swagger config 수정

--- BackEnd/src/main/java/com/orange/fintech/config/SwaggerConfig.java
+++ BackEnd/src/main/java/com/orange/fintech/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.orange.fintech.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
@@ -13,20 +14,27 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+
+        Info info =  new Info()
+                .title("Team Orange API")
+                .description("Spring Boot로 개발하는 RESTful API 명세서 입니다.")
+                .version("1.0.0");
+
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY)
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        Components components = new Components()
+                .addSecuritySchemes(
+                        "Bearer Token", securityScheme);
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("Bearer Token");
+
         return new OpenAPI()
-                .info(
-                        new Info()
-                                .title("Team Orange API")
-                                .description("Spring Boot로 개발하는 RESTful API 명세서 입니다.")
-                                .version("1.0.0"))
-                .components(
-                        new Components()
-                                .addSecuritySchemes(
-                                        "Authorization",
-                                        new io.swagger.v3.oas.models.security.SecurityScheme()
-                                                .type(SecurityScheme.Type.HTTP)
-                                                .scheme("bearer")
-                                                .bearerFormat("JWT")));
+                .info(info)
+                .components(components)
+                .addSecurityItem(securityRequirement);
     }
 
     @Bean
