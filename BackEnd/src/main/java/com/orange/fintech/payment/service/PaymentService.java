689dedbe0699c31125fb7d8ce6a2df9650f74df2
김신영 <estumav@gmail.com>
2024-03-20 09:30:26 +0900

    [feat] 메모 기능 추가

--- BackEnd/src/main/java/com/orange/fintech/payment/service/PaymentService.java
+++ BackEnd/src/main/java/com/orange/fintech/payment/service/PaymentService.java
@@ -4,10 +4,13 @@ import com.orange.fintech.group.entity.Group;
 import com.orange.fintech.member.entity.Member;
 import com.orange.fintech.payment.dto.TransactionDto;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public interface PaymentService {
 
     List<TransactionDto> getMyTransaction(Member member, Group group);
 
     boolean changeContainStatus(int transactionId, int groupId);
+
+    void memo(int transactionId, String memo) throws NoSuchElementException;
 }
