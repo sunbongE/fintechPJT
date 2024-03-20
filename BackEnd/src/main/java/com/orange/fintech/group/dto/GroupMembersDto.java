aeb24252233f5e37aee28fe077a8a8d94a5b1bbe
박태호 <qkrxogh7@naver.com>
2024-03-20 22:33:49 +0900

    feat: group에 포함된 인원 목록 조회

---
+++ BackEnd/src/main/java/com/orange/fintech/group/dto/GroupMembersDto.java
@@ -0,0 +1,16 @@
+package com.orange.fintech.group.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class GroupMembersDto {
+
+    String name;
+    String kakaoId;
+    String thumbnailImage;
+
+}
--- BackEnd/src/main/java/com/orange/fintech/group/repository/GroupQueryRepository.java
+++ BackEnd/src/main/java/com/orange/fintech/group/repository/GroupQueryRepository.java
@@ -2,16 +2,24 @@ package com.orange.fintech.group.repository;
 
 import static com.orange.fintech.group.entity.QGroup.*;
 import static com.orange.fintech.group.entity.QGroupMember.*;
+import static com.orange.fintech.member.entity.QMember.*;
 
+import com.orange.fintech.group.dto.GroupMembersDto;
 import com.orange.fintech.group.entity.Group;
+import com.orange.fintech.member.entity.Member;
+import com.orange.fintech.member.entity.QMember;
 import com.orange.fintech.member.repository.MemberRepository;
+import com.querydsl.core.Tuple;
 import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Repository
 @RequiredArgsConstructor
 @Transactional
@@ -42,5 +50,20 @@ public class GroupQueryRepository {
                                 .eq(kakaoId)
                                 .and(groupMember.state.eq(true)))
                 .fetch();
+    }
+
+    public List<GroupMembersDto> findGroupMembers(int groupId, String memberId) {
+
+        List<GroupMembersDto> groupList = queryFactory
+                .select(
+                        Projections.bean(
+                                GroupMembersDto.class,
+                                member.kakaoId, member.name, member.thumbnailImage))
+                .from(member)
+                .leftJoin(groupMember)
+                .on(groupMember.groupMemberPK.member.kakaoId.eq(member.kakaoId))
+                .where(groupMember.groupMemberPK.group.groupId.eq(groupId))
+                .fetch();
+        return groupList;
     }
 }
