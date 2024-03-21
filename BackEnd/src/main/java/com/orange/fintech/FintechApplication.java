09c6e489807b1fecab79cc507b7dc4b5b3c344c4
김신영 <estumav@gmail.com>
2024-03-21 17:16:32 +0900

    chore: spotless 적용

--- BackEnd/src/main/java/com/orange/fintech/FintechApplication.java
+++ BackEnd/src/main/java/com/orange/fintech/FintechApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FintechApplication {
-    //에러 1) java.net.SocketException: Network is unreachable (https://velog.io/@mingsound21/SpringBoot-AWS-S3%EB%A1%9C-%EC%9D%B4%EB%AF%B8%EC%A7%80-%EC%97%85%EB%A1%9C%EB%93%9C)
+    // 에러 1) java.net.SocketException: Network is unreachable
+    // (https://velog.io/@mingsound21/SpringBoot-AWS-S3%EB%A1%9C-%EC%9D%B4%EB%AF%B8%EC%A7%80-%EC%97%85%EB%A1%9C%EB%93%9C)
     static {
         System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
     }
