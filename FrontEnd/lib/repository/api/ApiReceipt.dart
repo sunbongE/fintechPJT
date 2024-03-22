bb17be0268fa93a9d12cca02884f44695e6320a1
김민준 <yid0816@naver.com>
2024-03-22 13:34:35 +0900

    [feat] 소셜 로그인 긴급 수정 및 영수증 사진 등록 코드 작성

---
+++ FrontEnd/lib/repository/api/ApiReceipt.dart
@@ -0,0 +1,23 @@
+import 'package:dio/dio.dart';
+import 'package:front/repository/commons.dart';
+
+final api = ApiClient();
+
+Future<Response?> postReceiptImage(FormData formData) async {
+  var dio = Dio();
+  try {
+    final res = await dio.post(
+      'https://0xk6maip7m.apigw.ntruss.com/custom/v1/29150/799498f1feeb274ee00714974948725693d0eb995f5478622e975cd57c51c4f7/document/receipt',
+      data: formData,
+      options: Options(
+        headers: {
+          'X-OCR-SECRET': 'eGduV3FSVHNaZklGeGxVT1J4R1drd2diT1pMeGxiT0o=',
+        },
+      ),
+    );
+    return res;
+  } catch (err) {
+    print("Error posting image: $err");
+    return null;
+  }
+}
