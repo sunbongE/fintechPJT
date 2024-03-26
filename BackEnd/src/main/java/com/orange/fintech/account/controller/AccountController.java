0cb25965419f1a255d8782b00dbe07eaf6ac89f9
박태호 <qkrxogh7@naver.com>
2024-03-27 00:56:48 +0900

    refactor: controller url

--- BackEnd/src/main/java/com/orange/fintech/account/controller/AccountController.java
+++ BackEnd/src/main/java/com/orange/fintech/account/controller/AccountController.java
@@ -48,7 +48,7 @@ public class AccountController {
         }
     }
 
-    @PutMapping("/list")
+    @PutMapping("/update")
     @Operation(
             summary = "주계좌 등록(수정)",
             description = "<string>회원의 <strong>주계좌</strong>를 등록 혹은 수정한다.")
@@ -63,6 +63,27 @@ public class AccountController {
         try {
             accountService.accountMainAccount(memberId, dto);
             return ResponseEntity.ok().body("주계좌 등록됨");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().body(BaseResponseBody.of(500, "서버 에러"));
+        }
+    }
+
+    @GetMapping("/transaction")
+    @Operation(summary = "회원의 계좌 목록 조회", description = "<string>회원의 <strong>계좌 목록</strong>을 조회한다.")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "성공"),
+        @ApiResponse(responseCode = "500", description = "서버 에러")
+    })
+    public ResponseEntity<?> findAllTransation(
+            @PathVariable("bankId") String bankId, Principal principal) {
+        if (!bankId.equals("001")) {
+            return ResponseEntity.ok().body(Collections.EMPTY_LIST);
+        }
+        String memberId = principal.getName();
+        try {
+            List<JSONObject> result = accountService.findAccountList(memberId);
+            return ResponseEntity.ok().body(result);
         } catch (Exception e) {
             e.printStackTrace();
             return ResponseEntity.internalServerError().body(BaseResponseBody.of(500, "서버 에러"));
