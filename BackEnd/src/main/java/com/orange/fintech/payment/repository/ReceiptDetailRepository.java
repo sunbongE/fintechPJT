f90fab347b3e3a060ceb202588a9f4deb6aa817e
김신영 <estumav@gmail.com>
2024-03-29 23:50:33 +0900

    [feat] 영수증디테일 추가

--- BackEnd/src/main/java/com/orange/fintech/payment/repository/ReceiptDetailRepository.java
+++ BackEnd/src/main/java/com/orange/fintech/payment/repository/ReceiptDetailRepository.java
@@ -2,12 +2,16 @@ package com.orange.fintech.payment.repository;
 
 import com.orange.fintech.payment.entity.Receipt;
 import com.orange.fintech.payment.entity.ReceiptDetail;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface ReceiptDetailRepository extends JpaRepository<ReceiptDetail, Integer> {
+
+    List<ReceiptDetail> findByReceipt(Receipt receipt);
+
     @Modifying
     @Query("DELETE FROM ReceiptDetail rd WHERE rd.receipt = :receipt")
     void deleteRecordsByReceipt(@Param("receipt") Receipt receipt);
