94300f0cc1a97d3ae96fbcaaa8324401fcf81f49
김신영 <estumav@gmail.com>
2024-03-30 19:11:01 +0900

    feat: test 코드 추가

--- BackEnd/src/main/java/com/orange/fintech/auth/controller/AuthController.java
+++ BackEnd/src/main/java/com/orange/fintech/auth/controller/AuthController.java
@@ -6,9 +6,13 @@ import com.orange.fintech.common.BaseResponseBody;
 import com.orange.fintech.jwt.JWTUtil;
 import com.orange.fintech.member.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -60,9 +64,20 @@ public class AuthController {
         return ResponseEntity.ok(BaseResponseBody.of(500, "로그아웃 실패"));
     }
 
+    @GetMapping("/test/token")
+    @Operation(summary = "jwt token 생성기", description = "token 생성")
+    public ResponseEntity<?> token(
+            @Parameter(description = "name") @RequestParam String name,
+            @Parameter(description = "kakao_id") @RequestParam String kakaoId) {
+        String accessToken =
+                jWTUtil.createAccessToken(
+                        name, kakaoId, Date.from(Instant.now().plus(30, ChronoUnit.DAYS)));
+
+        return ResponseEntity.status(200).body(accessToken);
+    }
+
     @GetMapping("/test")
     public ResponseEntity<?> test() {
-
         return ResponseEntity.ok(BaseResponseBody.of(200, "연결댐"));
     }
 }
