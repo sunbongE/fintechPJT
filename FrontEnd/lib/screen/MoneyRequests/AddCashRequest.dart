5dac140ebe1441471fb90aefbc81d8c017be8604
김지연 <flyhigh9870@gmail.com>
2024-04-03 14:15:42 +0900

    [feat] 정산을 수정하고 라우터 pop하면 다시 api 호출

--- FrontEnd/lib/screen/MoneyRequests/AddCashRequest.dart
+++ FrontEnd/lib/screen/MoneyRequests/AddCashRequest.dart
@@ -18,6 +18,7 @@ import '../../models/button/SizedButton.dart';
 import '../../repository/api/ApiGroup.dart';
 import '../../repository/api/ApiMoneyRequest.dart';
 import '../../utils/RequestModifyUtil.dart';
+import '../MoneyRequest.dart';
 
 class AddCashRequest extends StatefulWidget {
   final int groupId;
@@ -91,142 +92,167 @@ class _AddCashRequestState extends State<AddCashRequest> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('현금계산 항목 추가'),
-        scrolledUnderElevation: 0,
-        actions: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8),
-            child: SizedButton(
-              btnText: '완료',
-              size: ButtonSize.xs,
-              borderRadius: 10,
-              onPressed: _isFormFilled()
-                  ? () async {
-                      final groupId = widget.groupId;
-
-                      List<RequestMember> newMembers = List<RequestMember>.generate(requestCash.members.length, (index) {
-                        return RequestMember(
-                          memberId: requestCash.members[index].memberId,
-                          profileUrl: requestCash.members[index].profileUrl,
-                          name: requestCash.members[index].name,
-                          amount: amountList[index],
-                          lock: isLockList[index],
-                        );
-                      });
-
-                      RequestCash newRequestCash = RequestCash(
-                          transactionSummary: _titleController.text,
-                          location: _locationController.text,
-                          transactionBalance: int.tryParse(_priceController.text.replaceAll(',', '')) ?? 0,
-                          transactionDate: requestCash.transactionDate,
-                          transactionTime: requestCash.transactionTime,
-                          members: newMembers,
-                          remainder: remainderAmount);
-
-                      final data = newRequestCash.toJson();
-
-                      try {
-                        final response = await postAddCash(groupId, data);
-                        print('현금 등록 post 요청 성공: $response');
-                        Navigator.pop(context, true);
-                      } catch (e) {
-                        print('오류: $e');
-                      }
-                    }
-                  : null,
-              enable: _isFormFilled(),
-            ),
+    return WillPopScope(
+      onWillPop: () async {
+        Navigator.pop(context);
+        await Future.delayed(Duration.zero);
+        Navigator.pushReplacement(
+          context,
+          PageRouteBuilder(
+            pageBuilder: (context, animation, secondaryAnimation) => MoneyRequest(groupId: widget.groupId),
+            transitionsBuilder: (context, animation, secondaryAnimation, child) {
+              var begin = Offset(-1.0, 0.0);
+              var end = Offset.zero;
+              var curve = Curves.ease;
+              var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+              var offsetAnimation = animation.drive(tween);
+              return SlideTransition(
+                position: offsetAnimation,
+                child: child,
+              );
+            },
+            transitionDuration: Duration(milliseconds: 300),
           ),
-        ],
-      ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(20),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            TextField(
-              controller: _titleController,
-              decoration: InputDecoration(
-                labelText: '제목',
-                hintText: '제목을 입력하세요',
-                border: OutlineInputBorder(),
+        );
+        return false;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('현금계산 항목 추가'),
+          scrolledUnderElevation: 0,
+          actions: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: SizedButton(
+                btnText: '완료',
+                size: ButtonSize.xs,
+                borderRadius: 10,
+                onPressed: _isFormFilled()
+                    ? () async {
+                        final groupId = widget.groupId;
+
+                        List<RequestMember> newMembers = List<RequestMember>.generate(requestCash.members.length, (index) {
+                          return RequestMember(
+                            memberId: requestCash.members[index].memberId,
+                            profileUrl: requestCash.members[index].profileUrl,
+                            name: requestCash.members[index].name,
+                            amount: amountList[index],
+                            lock: isLockList[index],
+                          );
+                        });
+
+                        RequestCash newRequestCash = RequestCash(
+                            transactionSummary: _titleController.text,
+                            location: _locationController.text,
+                            transactionBalance: int.tryParse(_priceController.text.replaceAll(',', '')) ?? 0,
+                            transactionDate: requestCash.transactionDate,
+                            transactionTime: requestCash.transactionTime,
+                            members: newMembers,
+                            remainder: remainderAmount);
+
+                        final data = newRequestCash.toJson();
+
+                        try {
+                          final response = await postAddCash(groupId, data);
+                          print('현금 등록 post 요청 성공: $response');
+                          Navigator.pop(context, true);
+                        } catch (e) {
+                          print('오류: $e');
+                        }
+                      }
+                    : null,
+                enable: _isFormFilled(),
               ),
             ),
-            SizedBox(height: 10),
+          ],
+        ),
+        body: SingleChildScrollView(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              TextField(
+                controller: _titleController,
+                decoration: InputDecoration(
+                  labelText: '제목',
+                  hintText: '제목을 입력하세요',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 10),
 
-            // 지도에서 마커로 가져와서 장소 value 입력하게 해둠~~
-            // 근데.. 밑에 가격입력하는 창이 너무 눈에 안띔.. 일단 난 못찾았었어. 처음에 . 코드보고 알았음..
-            Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: _locationController,
-                    decoration: InputDecoration(
-                      labelText: '장소',
-                      hintText: '장소를 입력하세요',
-                      border: OutlineInputBorder(),
-                      suffixIcon: IconButton(
-                        icon: Icon(Icons.map),
-                        onPressed: () async {
-                          FocusScope.of(context).unfocus();
-                          final result = await Navigator.of(context).push(
-                            MaterialPageRoute(
-                              builder: (context) => SelectLocation(),
-                            ),
-                          );
-                          if (result is String) {
-                            _locationController.text = result;
-                          }
-                        },
+              // 지도에서 마커로 가져와서 장소 value 입력하게 해둠~~
+              // 근데.. 밑에 가격입력하는 창이 너무 눈에 안띔.. 일단 난 못찾았었어. 처음에 . 코드보고 알았음..
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _locationController,
+                      decoration: InputDecoration(
+                        labelText: '장소',
+                        hintText: '장소를 입력하세요',
+                        border: OutlineInputBorder(),
+                        suffixIcon: IconButton(
+                          icon: Icon(Icons.map),
+                          onPressed: () async {
+                            FocusScope.of(context).unfocus();
+                            final result = await Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => SelectLocation(),
+                              ),
+                            );
+                            if (result is String) {
+                              _locationController.text = result;
+                            }
+                          },
+                        ),
                       ),
                     ),
                   ),
+                ],
+              ),
+
+              SizedBox(height: 10),
+              AddCashAmountInputField(
+                controller: _priceController,
+                onSubmitted: (String) {}, labelText: '가격', hintText: 'ex)10000',
+              ),
+              if (requestCash.members?.isNotEmpty ?? false) ...[
+                Flexible(
+                  fit: FlexFit.loose,
+                  child: SizedBox(
+                    height: 400.h,
+                    child: RequestMemberList(
+                        requestDetail: requestCash,
+                        allSettledCallback: (bool value) {},
+                        callbackAmountList: (List<int> value) {
+                          setState(() {
+                            amountList = value;
+                            amountList = reCalculateAmount(requestCash.transactionBalance, amountList, isLockList);
+                            print(amountList);
+                            remainderAmount = reCalculateRemainder(requestCash.transactionBalance, amountList);
+                            print(remainderAmount);
+                          });
+                        },
+                        amountList: amountList,
+                        isLockList: (List<bool> value) {}),
+                  ),
+                ),
+                MoneyRequestDetailBottom(
+                  amount: remainderAmount,
+                ),
+              ] else ...[
+                Flexible(
+                  fit: FlexFit.loose,
+                  child: Center(
+                    child: Lottie.asset('assets/lotties/orangewalking.json'),
+                  ),
                 ),
               ],
-            ),
-
-            SizedBox(height: 10),
-            AddCashAmountInputField(
-              controller: _priceController,
-              onSubmitted: (String) {}, labelText: '가격', hintText: 'ex)10000',
-            ),
-            if (requestCash.members?.isNotEmpty ?? false) ...[
-              Flexible(
-                fit: FlexFit.loose,
-                child: SizedBox(
-                  height: 400.h,
-                  child: RequestMemberList(
-                      requestDetail: requestCash,
-                      allSettledCallback: (bool value) {},
-                      callbackAmountList: (List<int> value) {
-                        setState(() {
-                          amountList = value;
-                          amountList = reCalculateAmount(requestCash.transactionBalance, amountList, isLockList);
-                          print(amountList);
-                          remainderAmount = reCalculateRemainder(requestCash.transactionBalance, amountList);
-                          print(remainderAmount);
-                        });
-                      },
-                      amountList: amountList,
-                      isLockList: (List<bool> value) {}),
-                ),
-              ),
-              MoneyRequestDetailBottom(
-                amount: remainderAmount,
-              ),
-            ] else ...[
-              Flexible(
-                fit: FlexFit.loose,
-                child: Center(
-                  child: Lottie.asset('assets/lotties/orangewalking.json'),
-                ),
-              ),
             ],
-          ],
+          ),
         ),
       ),
     );
--- FrontEnd/lib/screen/MoneyRequests/MoneyRequestDetail.dart
+++ FrontEnd/lib/screen/MoneyRequests/MoneyRequestDetail.dart
@@ -7,6 +7,7 @@ import 'package:lottie/lottie.dart';
 
 import '../../components/addreceipt/AddReceipt.dart';
 import '../../components/moneyrequests/MoneyRequestDetailBottom.dart';
+import '../MoneyRequest.dart';
 import 'ReceiptView.dart';
 import '../../const/colors/Colors.dart';
 import '../../entities/RequestDetailModifyRequest.dart';
@@ -82,103 +83,128 @@ class _MoneyRequestDetailState extends State<MoneyRequestDetail> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.expense.transactionSummary),
-        scrolledUnderElevation: 0,
-        actions: [
-          if (isEditable)
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8),
-              child: SizedButton(
-                btnText: '수정',
-                size: ButtonSize.xs,
-                borderRadius: 10,
-                onPressed: () => navigateToMoneyRequestModify(),
-              ),
-            ),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            MoneyRequestItem(
-              expense: widget.expense,
-              isToggle: false, groupId: widget.groupId, clickable: false,
-            ),
-            if (request.members.isNotEmpty) ...[
-              SizedBox(
-                width: 300.w,
-                child: Text(
-                  '${request.memo}',
-                  overflow: TextOverflow.ellipsis,
+    return WillPopScope(
+      onWillPop: () async {
+        Navigator.pop(context);
+        await Future.delayed(Duration.zero);
+        Navigator.pushReplacement(
+          context,
+          PageRouteBuilder(
+            pageBuilder: (context, animation, secondaryAnimation) => MoneyRequest(groupId: widget.groupId),
+            transitionsBuilder: (context, animation, secondaryAnimation, child) {
+              var begin = Offset(-1.0, 0.0);
+              var end = Offset.zero;
+              var curve = Curves.ease;
+              var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+              var offsetAnimation = animation.drive(tween);
+              return SlideTransition(
+                position: offsetAnimation,
+                child: child,
+              );
+            },
+            transitionDuration: Duration(milliseconds: 300),
+          ),
+        );
+        return false;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.expense.transactionSummary),
+          scrolledUnderElevation: 0,
+          actions: [
+            if (isEditable)
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                child: SizedButton(
+                  btnText: '수정',
+                  size: ButtonSize.xs,
+                  borderRadius: 10,
+                  onPressed: () => navigateToMoneyRequestModify(),
                 ),
               ),
-              Padding(padding: EdgeInsets.symmetric(vertical: 7.w)),
-              SizedButton(
-                btnText: widget.expense.receiptEnrolled ? '영수증 보기' : '영수증 등록',
-                size: ButtonSize.l,
-                  onPressed: () async {
-                    if (widget.expense.receiptEnrolled) {
-                      final result = await Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => ReceiptView(expense: widget.expense, groupId: widget.groupId, expenseDetail: request)),
-                      );
-                      if (result == true) {
-                        fetchMyGroupPaymentsDetail();
-                      }
-                    } else {
-                      final result = await Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => AddReceipt(groupId: widget.groupId)),
-                      );
-                      if (result == true) {
-                        fetchMyGroupPaymentsDetail();
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              MoneyRequestItem(
+                expense: widget.expense,
+                isToggle: false, groupId: widget.groupId, clickable: false,
+              ),
+              if (request.members.isNotEmpty) ...[
+                SizedBox(
+                  width: 300.w,
+                  child: Text(
+                    '${request.memo}',
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                Padding(padding: EdgeInsets.symmetric(vertical: 7.w)),
+                SizedButton(
+                  btnText: widget.expense.receiptEnrolled ? '영수증 보기' : '영수증 등록',
+                  size: ButtonSize.l,
+                    onPressed: () async {
+                      if (widget.expense.receiptEnrolled) {
+                        final result = await Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => ReceiptView(expense: widget.expense, groupId: widget.groupId, expenseDetail: request)),
+                        );
+                        if (result == true) {
+                          fetchMyGroupPaymentsDetail();
+                        }
+                      } else {
+                        final result = await Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => AddReceipt(groupId: widget.groupId)),
+                        );
+                        if (result == true) {
+                          fetchMyGroupPaymentsDetail();
+                        }
                       }
                     }
-                  }
-              ),
-              Expanded(
-                child: SizedBox(
-                  //width: 368.w,
-                  height: 400.h,
-                  child: RequestMemberList(
-                    requestDetail: request,
-                    amountList: amounts,
-                    allSettledCallback: updateIsSettledStates,
-                    callbackAmountList: (List<int> value) { //amounts = value;
-                      setState(() {
-                        amounts = value;
-                        amounts = reCalculateAmount(
-                            widget.expense.transactionBalance, amounts,
-                            isLockedList);
-                        remainderAmount = reCalculateRemainder(
-                            widget.expense.transactionBalance, amounts);
-                        sendPutRequest();
-                      });
-                    },
-                    isLockList: (List<bool> value) {
-                      isLockedList = value;
-                    },
-                  ),
                 ),
-              ),
-              Padding(padding: EdgeInsets.symmetric(vertical: 10.w)),
-              MoneyRequestDetailBottom(
-                amount: remainderAmount,
-              ),
-            ] else
-              ...[
                 Expanded(
-                  child: Center(
-                    child: Lottie.asset('assets/lotties/orangewalking.json'),
+                  child: SizedBox(
+                    //width: 368.w,
+                    height: 400.h,
+                    child: RequestMemberList(
+                      requestDetail: request,
+                      amountList: amounts,
+                      allSettledCallback: updateIsSettledStates,
+                      callbackAmountList: (List<int> value) { //amounts = value;
+                        setState(() {
+                          amounts = value;
+                          amounts = reCalculateAmount(
+                              widget.expense.transactionBalance, amounts,
+                              isLockedList);
+                          remainderAmount = reCalculateRemainder(
+                              widget.expense.transactionBalance, amounts);
+                          sendPutRequest();
+                        });
+                      },
+                      isLockList: (List<bool> value) {
+                        isLockedList = value;
+                      },
+                    ),
                   ),
                 ),
-              ],
-            Padding(padding: EdgeInsets.symmetric(vertical: 10.w)),
-          ],
+                Padding(padding: EdgeInsets.symmetric(vertical: 10.w)),
+                MoneyRequestDetailBottom(
+                  amount: remainderAmount,
+                ),
+              ] else
+                ...[
+                  Expanded(
+                    child: Center(
+                      child: Lottie.asset('assets/lotties/orangewalking.json'),
+                    ),
+                  ),
+                ],
+              Padding(padding: EdgeInsets.symmetric(vertical: 10.w)),
+            ],
+          ),
         ),
       ),
     );
